d7b176f72d10a68bc16f421ce01315ffb87928f6
Akio Tamashiro <angelakio.tamashiro@gmail.com>
2021-11-28 20:55:05 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const WS_PORT = 3000
+
+
+app.use(express.json());
+
+const io = new Server(httpServer, {
+    cors: {
+      origin: ['https://web-stockeanding.herokuapp.com','https://api-stockeanding.herokuapp.com'],
+      methods: ['GET', 'POST'],
+      transports: ['websocket', 'polling'],
+      credentials: true
+    },
+    allowEIO3: true
+  });
+
+io.on("connection", (socket) => {
+  socket.on('join room', (room) => {
+    socket.join(room);
+  });
+});
+
+app.io = io;
+
+app.post("/send-msg-to-user", (req, res) =>{
+    const {body, username} = req.body;
+    const message = {
+        body : body,
+        date : new Date()
+    }
+
+    req.app.io.to(`user:${username}`).emit('notifications', message);
+
+    return res.status(200).send();
+});
+
+httpServer.listen(WS_PORT, () => {
+    console.log('listening on port 3000');
+});
